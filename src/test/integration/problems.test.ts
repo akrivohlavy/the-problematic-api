e83f1459e912107c831a6a2693b71a014f87393b
Antonín Křivohlavý <antonin.krivohlavy@iflix.com>
2019-09-27 13:29:25 +0200

    test(api): integration test for basic auth

--- src/test/integration/problems.test.ts
+++ src/test/integration/problems.test.ts
@@ -13,13 +13,46 @@ const aBetterProblemMock = {
     query: 'Where’s the peck of pickled peppers Peter Piper picked?',
 };
 
+const userMock = {
+    username: 'user1',
+    password: 'pass',
+};
+
+const otherUserMock = {
+    username: 'user2',
+    password: 'pass',
+};
+
 describe('Problems (Integration)', () => {
+    describe('API', () => {
+        it('is protected by Basic Auth', async () => {
+            await request(app)
+                .get('/api/problems')
+                .send(createProblemMock)
+                .expect(401)
+                .then(({ headers }: any) => {
+                    expect(headers).toHaveProperty('www-authenticate');
+                    expect(headers['www-authenticate']).toStartWith('Basic');
+                });
+
+            await request(app)
+                .get('/api/problems')
+                .auth(userMock.username, userMock.password)
+                .then(({ status }: any) => {
+                    expect(status)
+                        .toBeGreaterThanOrEqual(200)
+                        .toBeLessThanOrEqual(299);
+                });
+        });
+    });
+
     describe('User can', () => {
         let tempProblem: Problem;
 
         it('create a Problem', () => {
             return request(app)
                 .post('/api/problems')
+                .auth(userMock.username, userMock.password)
                 .send(createProblemMock)
                 .expect(200)
                 .then(({ body }: any) => {
@@ -31,6 +64,7 @@ describe('Problems (Integration)', () => {
         it('list Problems', () => {
             return request(app)
                 .get('/api/problems')
+                .auth(userMock.username, userMock.password)
                 .expect(200)
                 .then(({ body }: any) => {
                     expect(body).toMatchSnapshot();
@@ -40,6 +74,7 @@ describe('Problems (Integration)', () => {
         it('read Problem', () => {
             return request(app)
                 .get(`/api/problems/${tempProblem.id}`)
+                .auth(userMock.username, userMock.password)
                 .expect(200)
                 .then(({ body }: any) => {
                     expect(body).toEqual(tempProblem);
@@ -49,6 +84,7 @@ describe('Problems (Integration)', () => {
         it('update Problem', async () => {
             await request(app)
                 .put(`/api/problems/${tempProblem.id}`)
+                .auth(userMock.username, userMock.password)
                 .send(aBetterProblemMock)
                 .expect(200)
                 .then(({ body }: any) => {
@@ -58,17 +94,36 @@ describe('Problems (Integration)', () => {
 
             await request(app)
                 .put(`/api/problems/${tempProblem.id}`)
+                .auth(userMock.username, userMock.password)
                 .send(aBetterProblemMock)
                 .expect(304);
+        });
+
+        it('not modify Problem created by other user', async () => {
+            await request(app)
+                .put(`/api/problems/${tempProblem.id}`)
+                .auth(otherUserMock.username, otherUserMock.password)
+                .send(createProblemMock)
+                .expect(403);
+        });
+
+        it('not delete Problem created by other user', async () => {
+            await request(app)
+                .delete(`/api/problems/${tempProblem.id}`)
+                .auth(otherUserMock.username, otherUserMock.password)
+                .send(createProblemMock)
+                .expect(403);
         });
 
         it('delete Problem', async () => {
             await request(app)
                 .delete(`/api/problems/${tempProblem.id}`)
+                .auth(userMock.username, userMock.password)
                 .expect(204);
 
             return await request(app)
                 .get('/api/problems')
+                .auth(userMock.username, userMock.password)
                 .expect(204)
                 .then(({ body }: any) => {
                     expect(body).toBeEmpty();
