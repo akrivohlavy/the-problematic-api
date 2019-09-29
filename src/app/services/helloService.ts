21caa999f21a2407e9d8c5b636e7b15325d70d0e
Antonín Křivohlavý <antonin.krivohlavy@iflix.com>
2019-09-29 18:51:43 +0200

    chore: remove /hello

--- src/app/services/helloService.ts
+++
@@ -1,7 +0,0 @@
-import { HttpContext } from 'app/controllers/utils/httpContext';
-
-export const hello = (_params: any, context: HttpContext) => {
-    return { hello: 'World', payload: context.payload, user: context.user };
-};
-
-export const sum = (...xs: number[]) => xs.reduce((a, b) => a + b, 0);
