9d5f22d78d3ddfd3022241fc6a5e8ea30547339e
Antonín Křivohlavý <antonin.krivohlavy@gmail.com>
2020-03-03 15:14:56 +0100

    chore(server): create server asynchronously

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 import logger from 'app/logger';
 import config, { safeConfig } from 'config';
-import server from './server';
+import createServer from './server';
 
 logger.info(safeConfig, 'Loaded config');
-server
-    .listen(config.server.port)
-    .then(() => logger.info(`Server started, port=${config.server.port}`))
-    .catch((error: Error) => {
-        logger.error(`Server failed to start: ${error.stack}`);
-        process.exit(1);
-    });
-
-export default server;
+createServer().then((server: any) => {
+    return server
+        .listen(config.server.port)
+        .then(() => logger.info(`Server started, port=${config.server.port}`))
+        .catch((error: Error) => {
+            logger.error(`Server failed to start: ${error.stack}`);
+            process.exit(1);
+        });
+});
--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,9 @@ import logger from 'app/logger';
 import { createServer } from 'unicore';
 import bindRoutes from './config/routes';
 
-const server = createServer();
-server.use(logger.express);
-bindRoutes(server);
-
-export default server;
+export default async () => {
+    const server = createServer();
+    server.use(logger.express);
+    bindRoutes(server);
+    return server;
+};
